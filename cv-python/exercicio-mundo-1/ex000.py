9100b39c3d257e1bb0b1a54728ee8c29b9ed1fa3
amos <amosxrl@outlook.com>
2026-04-21 16:16:26 -0300

    ????

--- cv-python/exercicio-mundo-1/ex000.py
+++ cv-python/exercicio-mundo-1/ex000.py
@@ -1,4 +1,4 @@
 n1 = int(input('Coloque um número: '))
 pot = 2 ** n1
 
-print('2 elevado a {} é igual a {:.20f}! '.format(n1, pot))
+print('2 elevado a {} é igual a {:.0f}! '.format(n1, pot))
--- cv-python/extra/salario.py
+++ cv-python/extra/salario.py
@@ -4,9 +4,10 @@ while (aluno > 0):
     nota1 = int(input('Primeira nota: '))
     nota2 = int(input('Segunda nota: '))
     media = (nota1 + nota2)/2
-    aluno = aluno - 1
+    aluno = aluno -1
     posicao = 20 - aluno
     if media >= 6:
         print('{} {}, passo com {} na media.'.format(posicao, nome, media))
     else:
         print('{} {}, foi reprovado com {} na media.'.format(posicao, nome, media))
+  
